6323d7bd105f00910459cd1fd27a03c05cf3c2bf
Temirlan <zatovagul@gmail.com>
2022-04-27 11:21:05 +0300

    Localization configs fix

---
+++ lib/l10n/generated/ticandtoe_localizations_en.dart
@@ -0,0 +1,12 @@
+
+
+
+import 'ticandtoe_localizations.dart';
+
+/// The translations for English (`en`).
+class TicAndToeLocalizationsEn extends TicAndToeLocalizations {
+  TicAndToeLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get title => 'Tic and Toe';
+}
